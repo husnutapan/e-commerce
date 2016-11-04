8dea24f6c3938aafa7beae70b46ab4ff261a5052
husnutapan <husnu.tapan@gmail.com>
2016-11-04 14:02:47 +0200

    edit

--- src/com/ecommerce/controller/UserBean.java
+++ src/com/ecommerce/controller/UserBean.java
@@ -1,9 +1,15 @@
 package com.ecommerce.controller;
 
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import org.primefaces.context.RequestContext;
 
 import com.ecommerce.model.User;
 
+@ManagedBean
+@SessionScoped
 public class UserBean {
 
 	private User user;
@@ -15,6 +21,13 @@ public class UserBean {
 
 	public UserBean() {
 
+	}
+
+	public void userLogin() {
+		System.out.println(user.getEmail());
+		
+		// UPDATE FORM ON BEAN
+		RequestContext.getCurrentInstance().update("form");
 	}
 
 	public User getUser() {
