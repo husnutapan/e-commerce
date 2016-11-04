c21af0e663c9dc6bb4a8c966bb931ee1ff7a9340
husnutapan <husnu.tapan@gmail.com>
2016-11-04 11:25:10 +0200

    login and hibernate util class

--- src/com/ecommerce/controller/UserBean.java
+++ src/com/ecommerce/controller/UserBean.java
@@ -1,5 +1,28 @@
 package com.ecommerce.controller;
 
+import javax.annotation.PostConstruct;
+
+import com.ecommerce.model.User;
+
 public class UserBean {
+
+	private User user;
+
+	@PostConstruct
+	public void initialize() {
+		user = new User();
+	}
+
+	public UserBean() {
+
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 }
---
+++ src/com/ecommerce/model/User.java
@@ -0,0 +1,70 @@
+package com.ecommerce.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USER")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	private String telephone;
+	private String address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
